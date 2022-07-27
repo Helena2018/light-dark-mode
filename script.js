75df70b23bcfae8d4fb732fb52a1a3fc632f63a8
Helena2018 <helena2020liu@gmail.com>
2022-07-27 21:48:41 +1200

    locastrage set item

--- script.js
+++ script.js
@@ -34,10 +34,12 @@ function lightMode() {
 //Switch Theme Dynamically
 function switchTheme(event) {
   if(event.target.checked) {
-    document.documentElement.setAttribute('data-theme', 'dark')
+    document.documentElement.setAttribute('data-theme', 'dark');
+    localStorage.setItem('theme', 'dark')
     darkMode();
   } else {
-    document.documentElement.setAttribute('data-theme', 'light')
+    document.documentElement.setAttribute('data-theme', 'light');
+    localStorage.setItem('theme', 'dark')
     lightMode();
   }
 }
